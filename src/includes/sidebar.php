0bdc56b4dccc49c10bd54a511209aa020da99092
Luan <luanwolski2008@gmail.com>
2026-04-23 15:12:43 -0300

    user can now logout (need urgent fixes)

--- src/includes/sidebar.php
+++ src/includes/sidebar.php
@@ -1,77 +1,99 @@
 <?php
-
   $userRole = $_SESSION['role'] ?? null;
-  $userName = $_SESSION['name'] ?? null;
+  $userName = $_SESSION['name'] ?? 'Guest';
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="../assets/css/sidebar.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../assets/css/sidebar.css" rel="stylesheet">
 </head>
+
 <body>
 
-<div id="mySidenav" class="sidenav">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="../pages/homepage.php">Homepage</a>
-  <a href="#">Settings</a>
-  <a href="#">Contact</a>
+  <div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="../pages/homepage.php">Homepage</a>
+    <a href="#">Settings</a>
+    <a href="#">Contact</a>
 
-  <?php if( $userRole === 'admin'){
-      echo'
+    <?php if ($userRole === 'admin') {
+      echo '
       <a href="../pages/spot_register.php">Register spot</a>
       <a href="../pages/register.php">Register user</a>
       
       ';
     }
-  ?>
+    ?>
 
-  <?php if(session_status() === PHP_SESSION_ACTIVE): ?>
-    <a href="#" style=" ">
-      <span style="vertical-align: middle;">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-        </svg>  
-      </span>
-        <?php echo $userName ?>
-    </a>
-  <?php else: ?>
-    <a href="../pages/register.php" style="position: relative; bottom: 0;">
-      <span style="vertical-align: middle;">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-        </svg>
+    <?php if (session_status() === PHP_SESSION_ACTIVE): ?>
+      <?php echo'
+      <a href="#" style=" ">
+        <span style="vertical-align: middle;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" fill="currentColor"
+            class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+            <path fill-rule="evenodd"
+              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+          </svg>
         </span>
-          Create account
-    </a>
-  <?php endif; ?>
-  
-</div>
+        ' . $userName . '
+      </a>
 
-<div id="main">
-  <span style="font-size:30px;cursor:pointer; color:black; position: fixed;" onclick="openNav()"> 
-  
-  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
-  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
-  </svg>
+      <a href="../actions/logout.php" style="position: relative;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-box-arrow-left"
+          viewBox="0 0 16 16">
+          <path fill-rule="evenodd"
+            d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
+          <path fill-rule="evenodd"
+            d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
+        </svg>
+        Logout
+      </a>
+      ';
+      ?>
+    <?php else: ?>
+      <a href="../pages/register.php" style="position: relative; bottom: 0;">
+        <span style="vertical-align: middle;">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32px" height="32px" fill="currentColor"
+            class="bi bi-person-circle" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+            <path fill-rule="evenodd"
+              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+          </svg>
+        </span>
+        Create account
+      </a>
+    <?php endif; ?>
 
-  </span>
-</div>
+  </div>
 
-<script>
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
+  <div id="main">
+    <span style="font-size:30px;cursor:pointer; color:black; position: fixed;" onclick="openNav()">
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
-</script>
-   
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+        class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+        <path
+          d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+      </svg>
+
+    </span>
+  </div>
+
+  <script>
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    }
+
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+    }
+  </script>
+
 </body>
-</html> 
+
+</html>
---
+++ src/pages/spot_edit.php
@@ -0,0 +1,124 @@
+<?php
+    session_start();
+    include("../config/connect.php");
+
+    $userRole = $_SESSION['role'] ?? null;
+
+    if($userRole !== 'admin'){
+        echo'ACCESS DENIED!';
+    }
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+
+    <title>Register spot</title>
+
+</head>
+
+<body class="custom-bg-cojlor">
+
+    <div class="content" style="padding: 20px">
+
+        <h1 class="custom-success-txt-color"> Spot registration </h1>
+
+        <br>
+
+        <form class="client_register" method="post" action="../actions/create_spot.php" enctype="multipart/form-data">
+
+            <div class="row">
+
+                <div class="col-6">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="name" class="form-control" id="floatingInput"
+                            placeholder="name@example.com">
+                        <label for="floatingInput">Name</label>
+                    </div>
+                </div>
+
+                <div class="col-8">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="description" class="form-control" id="floatingInput"
+                            placeholder="name@example.com" autocomplete="off">
+                        <label for="floatingInput">Description</label>
+                    </div>
+                </div>
+
+                <div class="col-3">
+                    <div class="form-floating mb-3">
+                            <select class="form-select" name="type" id="type">
+								<option selected>Select the spot type</option>
+								<option value="Natural">Natural</option>
+                                <option value="Cultural">Cultural</option>
+                                <option value="Entertainement">Entertainement</option>
+                                <option value="Religious">Religious</option>
+							</select>
+						<label for="floatingSelect">Type</label>
+                    </div>       
+                </div>
+
+                <div class="col-6">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="city" class="form-control" id="floatingInput"
+                            placeholder="name@example.com">
+                        <label for="floatingInput">City</label>
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="state" class="form-control" id="floatingInput"
+                            placeholder="name@example.com">
+                        <label for="floatingInput">State</label>
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="country" class="form-control" id="floatingInput"
+                            placeholder="name@example.com">
+                        <label for="floatingInput">Country</label>
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="latitude" class="form-control" id="floatingInput"
+                            placeholder="name@example.com">
+                        <label for="floatingInput">Latitude</label>
+                    </div>
+                </div>
+
+                <div class="col-6">
+                    <div class="form-floating mb-3">
+                        <input type="text" name="longitude" class="form-control" id="floatingInput"
+                            placeholder="name@example.com">
+                        <label for="floatingInput">Longitude</label>
+                    </div>
+                </div>
+
+                <input type="file" id="picture" name="picture" accept="image/*">
+                <img id="previewImage" src="" alt="Image Preview" style="width: 200px; height: auto;">
+
+                <div class="col-12">
+                    <button type="input" class="btn btn-success">Resgiter spot</button>
+                </div>
+
+            </div>
+        </form>
+    </div>
+    
+</body>
+
+</html>
