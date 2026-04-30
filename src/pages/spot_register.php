0fb370879ada2e8b81f9939894d3c291c6ed3385
Luan <luanwolski2008@gmail.com>
2026-04-30 10:31:08 -0300

    fixed some layout problems

--- src/pages/spot_register.php
+++ src/pages/spot_register.php
@@ -28,134 +28,139 @@
 </head>
 
 <body>
-    <?php
-    include("../includes/fonts.php");
-    include("../includes/sidebar.php");
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-3">
-                <!-- Empty left column -->
-            </div>
-            <div class="col-sm-6">
-                <div class="spotRegister text-center">
-                    <div class="container">
-                        <br>
-                        <h1> <b> SP<i class="bi bi-compass"></i>T REGISTER</i> </b> </h1>
-
-                        <div class="content" style="padding: 10px">
-
+    <div class="layout">
+        <aside>
+            <?php
+                include("../includes/fonts.php");
+                include("../includes/sidebar.php");
+            ?>
+        </aside>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <!-- Empty left column -->
+                </div>
+                <div class="col-sm-6">
+                    <div class="spotRegister text-center">
+                        <div class="container">
                             <br>
+                            <h1> <b> SP<i class="bi bi-compass"></i>T REGISTER</i> </b> </h1>
 
-                            <form class="client_register" method="post" action="../actions/create_spot.php"
-                                enctype="multipart/form-data">
+                            <div class="content" style="padding: 10px">
 
-                                <div class="row">
+                                <br>
 
-                                    <div class="col-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" name="name" class="form-control" id="floatingInput"
-                                                placeholder="name@example.com">
-                                            <label for="floatingInput">Name</label>
+                                <form class="client_register" method="post" action="../actions/create_spot.php"
+                                    enctype="multipart/form-data">
+
+                                    <div class="row">
+
+                                        <div class="col-6">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="name" class="form-control" id="floatingInput"
+                                                    placeholder="name@example.com">
+                                                <label for="floatingInput">Name</label>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-6">
-                                        <div class="form-floating mb-3">
-                                            <select class="form-select" name="type" id="type">
-                                                <option selected>Select the spot type</option>
-                                                <option value="Natural">Natural</option>
-                                                <option value="Cultural">Cultural</option>
-                                                <option value="Entertainement">Entertainement</option>
-                                                <option value="Religious">Religious</option>
-                                            </select>
-                                            <label for="floatingSelect">Type</label>
+                                        <div class="col-6">
+                                            <div class="form-floating mb-3">
+                                                <select class="form-select" name="type" id="type">
+                                                    <option selected>Select the spot type</option>
+                                                    <option value="Natural">Natural</option>
+                                                    <option value="Cultural">Cultural</option>
+                                                    <option value="Entertainement">Entertainement</option>
+                                                    <option value="Religious">Religious</option>
+                                                </select>
+                                                <label for="floatingSelect">Type</label>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" name="description" class="form-control"
-                                                id="floatingInput" placeholder="name@example.com" autocomplete="off">
-                                            <label for="floatingInput">Description</label>
+                                        <div class="col-6">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="description" class="form-control"
+                                                    id="floatingInput" placeholder="name@example.com" autocomplete="off">
+                                                <label for="floatingInput">Description</label>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" name="city" class="form-control" id="floatingInput"
-                                                placeholder="name@example.com">
-                                            <label for="floatingInput">City</label>
+                                        <div class="col-6">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="city" class="form-control" id="floatingInput"
+                                                    placeholder="name@example.com">
+                                                <label for="floatingInput">City</label>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" name="state" class="form-control" id="floatingInput"
-                                                placeholder="name@example.com">
-                                            <label for="floatingInput">State</label>
+                                        <div class="col-6">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="state" class="form-control" id="floatingInput"
+                                                    placeholder="name@example.com">
+                                                <label for="floatingInput">State</label>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" name="country" class="form-control" id="floatingInput"
-                                                placeholder="name@example.com">
-                                            <label for="floatingInput">Country</label>
+                                        <div class="col-6">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="country" class="form-control" id="floatingInput"
+                                                    placeholder="name@example.com">
+                                                <label for="floatingInput">Country</label>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" name="latitude" class="form-control" id="floatingInput"
-                                                placeholder="name@example.com">
-                                            <label for="floatingInput">Latitude</label>
+                                        <div class="col-6">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="latitude" class="form-control" id="floatingInput"
+                                                    placeholder="name@example.com">
+                                                <label for="floatingInput">Latitude</label>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-6">
-                                        <div class="form-floating mb-3">
-                                            <input type="text" name="longitude" class="form-control" id="floatingInput"
-                                                placeholder="name@example.com">
-                                            <label for="floatingInput">Longitude</label>
+                                        <div class="col-6">
+                                            <div class="form-floating mb-3">
+                                                <input type="text" name="longitude" class="form-control" id="floatingInput"
+                                                    placeholder="name@example.com">
+                                                <label for="floatingInput">Longitude</label>
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-3">
-                                        <!-- Empty col to centralize the preview -->
-                                    </div>
-                                    <div class="col-6">
-                                        <div class="uploadPreview">
-                                            <img id="preview" src="../assets/media/preview.png" alt="Image Preview">
+                                        <div class="col-3">
+                                            <!-- Empty col to centralize the preview -->
                                         </div>
-                                        <br>
+                                        <div class="col-6">
+                                            <div class="uploadPreview">
+                                                <img id="preview" src="../assets/media/preview.png" alt="Image Preview">
+                                            </div>
+                                            <br>
+                                        </div>
+
+                                        <div class="col-3">
+                                            <!-- Empty col to centralize the preview -->
+                                        </div>
+
+                                        <div class="col-12">
+                                            <label class="uploadBtn">
+                                                <i class="bi bi-upload"></i>
+                                                <input type="file" id="fileInput" name="picture" accept="image/*">
+                                            </label>
+                                        </div>
+
+                                        <br><br><br>
+
+                                        <div class="col-12">
+                                            <button type="input" class="btn custom-blue">Register spot</button>
+                                        </div>
+
                                     </div>
-
-                                    <div class="col-3">
-                                        <!-- Empty col to centralize the preview -->
-                                    </div>
-
-                                    <div class="col-12">
-                                        <label class="uploadBtn">
-                                            <i class="bi bi-upload"></i>
-                                            <input type="file" id="fileInput" name="picture" accept="image/*">
-                                        </label>
-                                    </div>
-
-                                    <br><br><br>
-
-                                    <div class="col-12">
-                                        <button type="input" class="btn custom-blue">Register spot</button>
-                                    </div>
-
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-3">
-                <!-- Empty right column -->
+                <div class="col-sm-3">
+                    <!-- Empty right column -->
+                </div>
             </div>
         </div>
     </div>
