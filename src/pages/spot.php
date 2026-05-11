a7a996372cf66520696d7e6e9b913556dcea6c97
Luan <luanwolski2008@gmail.com>
2026-05-11 15:07:08 -0300

    started developing the commentary field

--- src/pages/spot.php
+++ src/pages/spot.php
@@ -19,6 +19,7 @@ if (!$query) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link href="../assets/css/spot.css" rel="stylesheet">
     <title>Produto</title>
 
@@ -92,6 +93,15 @@ if (!$query) {
 
                 </div>
             <?php } ?>
+        </div>
+
+        <div class="commentary-container">
+
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                <label>Add a comment...</label>
+            </div>
+
         </div>
     </div>
 </body>
