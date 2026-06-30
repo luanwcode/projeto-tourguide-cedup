a69e5f94661f62bc145b5a3137fdc6052d884069
Luan <luanwolski2008@gmail.com>
2026-06-30 14:21:28 -0300

    added email validation

--- src/auth/email_validate.php
+++ src/auth/email_validate.php
@@ -1,14 +1,16 @@
 <?php 
 
+    include("../config/connect.php");
+
     $email = $_POST['email'];
 
-    $sql_verify_email = "SELECT id_user FROM users WHERE email = ?";]
-    $stmt = $conn->prepare($sql_verify_email);
+    $sql_verify_email = "SELECT id_user FROM users WHERE email = ?";
+    $stmt = $connect->prepare($sql_verify_email);
     $stmt->bind_param("s", $email);
     $stmt->execute();
 
-    $result = $stmt->get-result();
+    $result = $stmt->get_result();
 
-    echo $result->num_rows > 0? "1" : "0";
+    echo $result->num_rows > 0 ? "1" : "0";
 
 ?>
--- src/pages/user_register.php
+++ src/pages/user_register.php
@@ -34,7 +34,7 @@
                     <form name="register" method="post" action="../actions/create_user.php">
                         <input class="form-control" type="name" name="name" placeholder="Your Name" required>
                         <br>
-                        <input class="form-control" type="email" name="email" placeholder="exemple@gmail.com" required>
+                        <input class="form-control" id="email" type="email" name="email" placeholder="exemple@gmail.com" required>
                         <br>
                         
                         <div class="row" style="justify-content: center;">
@@ -155,7 +155,7 @@
 						}
 					}
 					
-					// Adicionar evento de input para o campo de confirmação de senha
+					// Adds input event to the password confirmation field
 					document.getElementById('confirmpassword').addEventListener('input', checkPasswordMatch);
 
 					// Password visibility button
@@ -182,24 +182,6 @@
 					document.getElementById('toggleConfirmpassword').addEventListener('click', function() {
 						togglePassword('confirmpassword', 'toggleConfirmpassword');
 					});
-
-					//Email validation
-					const inputEmail = document.getElementById("email");
-
-					inputEmail.addEventListener("blue", async() => {
-						const result = await fetch("../auth/email_validate.php",{
-							method: "POST",
-							body: new URLSearchParams({
-								email: inputEmail.value;
-							})
-						});
-
-						const exists = await result.text();
-
-						if(exists === "1"){
-							alert("E-mail already registered");
-						}
-					});
 				</script>
 
                 </div>
@@ -210,5 +192,28 @@
         </div>
     </div>
 
+	<script>
+		//Email validation
+		const email = document.getElementById("email");
+
+		email.addEventListener("blur", async () => {
+			const result = await fetch("../auth/email_validate.php",{
+				method: "POST",
+				body: new URLSearchParams({
+					email: document.getElementById("email").value
+				})
+			});
+
+			console.log(result.status);
+			const exists = await result.text();
+
+			console.log(exists);
+
+			if(exists === "1"){
+				alert("E-mail already registered");
+				document.getElementById("email").value = "";
+			}
+		});
+	</script>
 
 </body>
